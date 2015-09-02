507a9a5d2cc41226ee327cab1bd37f7b7316838b
Abhishek K Das <abhishekdas1503@gmail.com>
2015-09-02 08:31:53 +0530

    Add starter code for MovieFragment
    Starter code the fragment inside main activity which
    contains the grid view.

---
+++ app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/MovieFragment.java
@@ -0,0 +1,31 @@
+package com.gmail.abhishekdas1503.email.popularmovies;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+/**
+ * Encapsulates fetching the movie details and displaying it as {@link GridView} layout
+ */
+public class MovieFragment extends Fragment {
+
+    public MovieFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+    }
+}
