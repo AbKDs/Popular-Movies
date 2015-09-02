e66d95bb120409e6c044e35c733f2c086bb04048
Abhishek K Das <abhishekdas1503@gmail.com>
2015-09-02 20:31:38 +0530

    Define abstract methods of BaseAdapter
    Defined the abstract methods of BaseAdapter {getView, getCount,
    getItem and getItemId} to complete the custom MovieAdapter Class.

--- app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/MovieAdapter.java
@@ -1,6 +1,12 @@
 package com.gmail.abhishekdas1503.email.popularmovies;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.List;
 
 /**
  * Custom implementation of Adapter {@link BaseAdapter}which will store
@@ -9,5 +15,54 @@ import android.widget.BaseAdapter;
  * name of the movie in the TextView below it
  */
 public class MovieAdapter extends BaseAdapter {
+    // Stores a list of movie objects
+    private List<Movie> mMovieList;
 
+    // Context of the caller method
+    private Context mContext;
+
+    // Layout Inflater from the context
+    private LayoutInflater mInflater;
+
+    public MovieAdapter(Context context, List<Movie> movieList) {
+        mMovieList = movieList;
+        mContext = context;
+
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return mMovieList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mMovieList.get(position);
+    }
+
+    /* Used when we need a Unique ID associated with our item.
+     * Generally when there are rows returned from a sql database.
+     * Not using this function. Have to override since it is
+     * abstract.
+     */
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        MovieView movieView;
+        if (convertView == null) {
+            movieView = (MovieView) mInflater.inflate(R.layout.grid_item_movie, null);
+        } else {
+            movieView = (MovieView) convertView;
+        }
+
+        // returns a movie object from the mMovieList
+        Movie movie= (Movie) getItem(position);
+        movieView.showMovie(movie);
+        return movieView;
+    }
 }
