2e9086f64f8cb46144122635ff69ebb7a8db3e04
Abhishek K Das <abhishekdas1503@gmail.com>
2015-09-02 08:32:37 +0530

    Add starter code for MovieAdapter
    Added starter code for custom implementation of Adapter
    which will contain the LinearLayout View as defined in
    grid_item_movie.xml

---
+++ app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/MovieAdapter.java
@@ -0,0 +1,13 @@
+package com.gmail.abhishekdas1503.email.popularmovies;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Custom implementation of Adapter {@link BaseAdapter}which will store
+ * LinearLayout views that contain one ImageView to display
+ * the image of Movie as downloaded from MovieDB API and
+ * name of the movie in the TextView below it
+ */
+public class MovieAdapter extends BaseAdapter {
+
+}
