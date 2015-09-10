adc94e4ee559946977707e8a1b27da6cf8e54618
Abhishek K Das <abhishekdas1503@gmail.com>
2015-09-10 05:53:55 +0530

    Add Picasso code in showMovie()
    Added Picasso code in showMovie() in MovieView which directly
    uses the uri provided by movieDB database to download the image
    into the required ImageView.

--- app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/MovieView.java
+++ app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/MovieView.java
@@ -1,22 +1,31 @@
 package com.gmail.abhishekdas1503.email.popularmovies;
 
 import android.content.Context;
-import android.graphics.Bitmap;
+import android.net.Uri;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.gmail.abhishekdas1503.email.popularmovies.model.Movie;
+import com.squareup.picasso.Picasso;
 
 /**
- * A custom implementaion of a view which extends from a LinearLayout
+ * A custom implementation of a view which extends from a LinearLayout
  * and is helps in avoid ViewHolder class by saving the views in the
  * member variables
  */
 public class MovieView extends LinearLayout {
 
     private ImageView mImageView;
+    private static final String MOVIE_BASE_URL = "http://image.tmdb.org/t/p";
+    private static final String SIZE = "w185";
+
+    private DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+    private float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
+    private float dpWidth  = displayMetrics.widthPixels / displayMetrics.density;
+
+    private static final String LOG_TAG = MovieView.class.getSimpleName();
 
     /** Inherited constructor. */
     public MovieView(Context context) {
@@ -41,8 +50,14 @@ public class MovieView extends LinearLayout {
 
     public void showMovie(Movie movie) {
         String path = movie.getPosterPath();
-        String movieName = movie.getName();
+        String name = movie.getTitle();
+        path = path.replaceFirst("/", "");
 
-        mImageView.setImageBitmap(poster);
+        Uri uri = Uri.parse(MOVIE_BASE_URL).buildUpon()
+                .appendPath(SIZE)
+                .appendPath(path)
+                .build();
+
+        Picasso.with(getContext()).load(uri).into(mImageView);
     }
 }
