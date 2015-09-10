5ec3ef13faba7a0279e733290e41d404c6f09e54
Abhishek K Das <abhishekdas1503@gmail.com>
2015-09-10 05:30:34 +0530

    Change Movie class for new member variables
    Stores the data related to a movie as downloaded from the
    movieDB database.

--- app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/model/Movie.java
+++ app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/model/Movie.java
@@ -1,33 +1,249 @@
 package com.gmail.abhishekdas1503.email.popularmovies.model;
 
-import android.graphics.Bitmap;
 
 /**
- * Moive class whose object will store the name of the movie
- * and bitmap (poster) downloaded from the movieDB database.
+ * Movie class whose object which stores all the details about a movie
+ * downloaded from the movieDB API.
  */
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Movie {
-    private Bitmap mPoster;
-    private String mName;
 
-    public Movie(Bitmap mPoster, String mName) {
-        this.mPoster = mPoster;
-        this.mName = mName;
+    @Expose
+    private Boolean adult;
+    @SerializedName("backdrop_path")
+    @Expose
+    private String backdropPath;
+    @SerializedName("genre_ids")
+    @Expose
+    private List<Integer> genreIds = new ArrayList<>();
+    @Expose
+    private Integer id;
+    @SerializedName("original_language")
+    @Expose
+    private String originalLanguage;
+    @SerializedName("original_title")
+    @Expose
+    private String originalTitle;
+    @Expose
+    private String overview;
+    @SerializedName("release_date")
+    @Expose
+    private String releaseDate;
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+    @Expose
+    private Double popularity;
+    @Expose
+    private String title;
+    @Expose
+    private Boolean video;
+    @SerializedName("vote_average")
+    @Expose
+    private Double voteAverage;
+    @SerializedName("vote_count")
+    @Expose
+    private Integer voteCount;
+
+    /**
+     * @return The adult
+     */
+    public Boolean getAdult() {
+        return adult;
     }
 
-    public Bitmap getPoster() {
-        return mPoster;
+    /**
+     * @param adult The adult
+     */
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
     }
 
-    public void setPoster(Bitmap mPoster) {
-        this.mPoster = mPoster;
+    /**
+     * @return The backdropPath
+     */
+    public String getBackdropPath() {
+        return backdropPath;
     }
 
-    public String getName() {
-        return mName;
+    /**
+     * @param backdropPath The backdrop_path
+     */
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
     }
 
-    public void setName(String mName) {
-        this.mName = mName;
+    /**
+     * @return The genreIds
+     */
+    public List<Integer> getGenreIds() {
+        return genreIds;
     }
+
+    /**
+     * @param genreIds The genre_ids
+     */
+    public void setGenreIds(List<Integer> genreIds) {
+        this.genreIds = genreIds;
+    }
+
+    /**
+     * @return The id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id The id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return The originalLanguage
+     */
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    /**
+     * @param originalLanguage The original_language
+     */
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    /**
+     * @return The originalTitle
+     */
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    /**
+     * @param originalTitle The original_title
+     */
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    /**
+     * @return The overview
+     */
+    public String getOverview() {
+        return overview;
+    }
+
+    /**
+     * @param overview The overview
+     */
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    /**
+     * @return The releaseDate
+     */
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    /**
+     * @param releaseDate The release_date
+     */
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    /**
+     * @return The posterPath
+     */
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    /**
+     * @param posterPath The poster_path
+     */
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    /**
+     * @return The popularity
+     */
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    /**
+     * @param popularity The popularity
+     */
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+    }
+
+    /**
+     * @return The title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title The title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return The video
+     */
+    public Boolean getVideo() {
+        return video;
+    }
+
+    /**
+     * @param video The video
+     */
+    public void setVideo(Boolean video) {
+        this.video = video;
+    }
+
+    /**
+     * @return The voteAverage
+     */
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    /**
+     * @param voteAverage The vote_average
+     */
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    /**
+     * @return The voteCount
+     */
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    /**
+     * @param voteCount The vote_count
+     */
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+
 }
