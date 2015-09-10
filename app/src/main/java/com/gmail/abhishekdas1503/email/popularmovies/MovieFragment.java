3919866ab89ad3d9068521af1cb5e0b6933b73bd
Abhishek K Das <abhishekdas1503@gmail.com>
2015-09-10 06:10:52 +0530

    Modify onCreateView in main fragment
    Added code which creates the main grid view of the movies as
    downloaded from tmdb.

--- app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/MovieFragment.java
+++ app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/MovieFragment.java
@@ -8,10 +8,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import com.gmail.abhishekdas1503.email.popularmovies.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Encapsulates fetching the movie details and displaying it as {@link GridView} layout
  */
 public class MovieFragment extends Fragment {
+    private MovieAdapter mMovieAdapter;
+    private List<Movie> mMovieList = new ArrayList<>();
 
     public MovieFragment() {
 
@@ -25,6 +32,26 @@ public class MovieFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return null;
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mMovieAdapter = new MovieAdapter(
+                getActivity(),    // Context of the app
+                mMovieList
+        );
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview_movies);
+        gridView.setAdapter(mMovieAdapter);
+
+        updateMovies();
+
+        return rootView;
     }
+
+    /**
+     * @param:void
+     * Helper function to download the content from movieDB and set it to the
+     * movieAdapter
+     */
+
+    void updateMovies() {}
 }
