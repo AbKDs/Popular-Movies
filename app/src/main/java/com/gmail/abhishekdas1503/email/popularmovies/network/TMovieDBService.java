df0ac07f026cdc6ace076f68bc5ff555a45a26a6
Abhishek K Das <abhishekdas1503@gmail.com>
2015-09-10 05:37:40 +0530

    Add movie response interface
    Movie API interface for the retrofit library. This interface helps in
    downloading the json data from the movieDB website.

---
+++ app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/network/TMovieDBService.java
@@ -0,0 +1,18 @@
+package com.gmail.abhishekdas1503.email.popularmovies.network;
+import com.gmail.abhishekdas1503.email.popularmovies.model.MovieResponse;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
+/**
+ * Movie API interface for the retrofit library. This interface helps in
+ * downloading the json data from the movieDB website.
+ */
+
+public interface TMovieDBService {
+    @GET("/movie")
+    void getMovieResponse(@Query("sort_by") String sortKey,
+                          @Query("api_key") String apiKey,
+                          Callback<MovieResponse> response);
+}
