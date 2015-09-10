5c2f3ca746e58d9e10612d6ed2f32ce8cb6534e5
Abhishek K Das <abhishekdas1503@gmail.com>
2015-09-10 06:18:23 +0530

    Add updateMovies() code in MovieFragment
    Added code in updateMovies() which actually downloads the movies
    using retrofit and calls addAll() method to modify the screen.

--- app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/MovieFragment.java
+++ app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/MovieFragment.java
@@ -7,11 +7,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import com.gmail.abhishekdas1503.email.popularmovies.model.Movie;
+import com.gmail.abhishekdas1503.email.popularmovies.model.MovieResponse;
+import com.gmail.abhishekdas1503.email.popularmovies.network.TMovieDBService;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 /**
  * Encapsulates fetching the movie details and displaying it as {@link GridView} layout
@@ -53,5 +61,26 @@ public class MovieFragment extends Fragment {
      * movieAdapter
      */
 
-    void updateMovies() {}
+    void updateMovies() {
+        final RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("http://api.themoviedb.org/3/discover")
+                .build();
+        String sortKey = "popularity.desc";
+        TMovieDBService service = restAdapter.create(TMovieDBService.class);
+
+        service.getMovieResponse(sortKey, ApiKey.API_KEY,
+                new Callback<MovieResponse>() {
+                    @Override
+                    public void success(MovieResponse movieResponse, Response response) {
+                        mMovieAdapter.addAll(movieResponse.getMovies());
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        // Toast for the moment
+                        // Appropriate error handling code should be present here
+                        Toast.makeText(getActivity(), "Failed !", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
 }
