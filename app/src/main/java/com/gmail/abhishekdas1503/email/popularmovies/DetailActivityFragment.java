a93ab0e99ea3905a933c4487a43850320948a34f
Abhishek K Das <abhishekdas1503@gmail.com>
2015-09-14 18:19:02 +0530

    Add code for displaying detail fragment
    Added code which displays the details of the movie on runtime.

--- app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/DetailActivityFragment.java
+++ app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/DetailActivityFragment.java
@@ -2,14 +2,16 @@ package com.gmail.abhishekdas1503.email.popularmovies;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.gmail.abhishekdas1503.email.popularmovies.model.Movie;
+import com.squareup.picasso.Picasso;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -32,11 +34,34 @@ public class DetailActivityFragment extends Fragment {
         Intent movieIntent = getActivity().getIntent();
         Movie movie = movieIntent.getExtras().getParcelable("movie");
 
-        TextView tv = (TextView) rootView.findViewById(R.id.textView);
-        Log.d("Reached here", "Oh yeah");
-        tv.setText(movie.getTitle() /*+ " " + movie.getOverview() + " "
-                + movie.getVoteAverage()*/);
-        Log.d("Couldn't Reached here", "Oh no");
+        TextView title = (TextView) rootView.findViewById(R.id.title);
+        title.setText(movie.getTitle());
+
+        ImageView poster = (ImageView) rootView.findViewById(R.id.poster);
+        String path = movie.getPosterPath();
+        if (path != null) {
+            path = path.replaceFirst("/", "");
+
+            Uri uri = Uri.parse(MovieView.MOVIE_BASE_URL).buildUpon()
+                    .appendPath(MovieView.SIZE)
+                    .appendPath(path)
+                    .build();
+
+            Picasso.with(getActivity()).load(uri).placeholder(R.drawable.placeholder).into(poster);
+        } else {
+            Picasso.with(getActivity()).load(R.drawable.placeholder).into(poster);
+        }
+
+
+        TextView release = (TextView) rootView.findViewById(R.id.release);
+        release.setText(movie.getReleaseDate().substring(0, 4));
+
+        TextView vote = (TextView) rootView.findViewById(R.id.vote);
+        vote.setText(movie.getVoteAverage() + "/10");
+
+        TextView synopsis = (TextView) rootView.findViewById(R.id.synopsis);
+        synopsis.setText(movie.getOverview());
+
         return rootView;
     }
 }
