c4c0b87f987ef9abc50ca774ebd0c72937147838
Abhishek K Das <abhishekdas1503@gmail.com>
2015-09-17 04:53:02 +0530

    Refactor to reuse MovieView instead of ImageView
    Refactored code so that custom defined movie view is used instead
    of imageview. This saves rewriting same code twice in two different
    places.

--- app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/DetailActivityFragment.java
+++ app/src/main/java/com/gmail/abhishekdas1503/email/popularmovies/DetailActivityFragment.java
@@ -2,16 +2,13 @@ package com.gmail.abhishekdas1503.email.popularmovies;
 
 import android.app.Fragment;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.gmail.abhishekdas1503.email.popularmovies.model.Movie;
-import com.squareup.picasso.Picasso;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -37,21 +34,8 @@ public class DetailActivityFragment extends Fragment {
         TextView title = (TextView) rootView.findViewById(R.id.title);
         title.setText(movie.getTitle());
 
-        ImageView poster = (ImageView) rootView.findViewById(R.id.poster);
-        String path = movie.getPosterPath();
-        if (path != null) {
-            path = path.replaceFirst("/", "");
-
-            Uri uri = Uri.parse(MovieView.MOVIE_BASE_URL).buildUpon()
-                    .appendPath(MovieView.SIZE)
-                    .appendPath(path)
-                    .build();
-
-            Picasso.with(getActivity()).load(uri).placeholder(R.drawable.placeholder).into(poster);
-        } else {
-            Picasso.with(getActivity()).load(R.drawable.placeholder).into(poster);
-        }
-
+        MovieView movieView = (MovieView) rootView.findViewById(R.id.poster);
+        movieView.showMovie(movie);
 
         TextView release = (TextView) rootView.findViewById(R.id.release);
         release.setText(movie.getReleaseDate().substring(0, 4));
